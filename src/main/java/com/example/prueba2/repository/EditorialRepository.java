def1c020a69145d35e5b52e09fa516e6d8e9e242
Onyvnn <onyvnn@gmail.com>
2026-05-09 22:17:13 -0400

    Repositorios listos

---
+++ src/main/java/com/example/prueba2/repository/EditorialRepository.java
@@ -0,0 +1,9 @@
+package com.example.prueba2.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.example.prueba2.model.Editorial;
+
+@Repository
+public interface EditorialRepository extends JpaRepository<Editorial, Integer> {
+}
---
+++ src/main/java/com/example/prueba2/repository/EnvioRepository.java
@@ -0,0 +1,10 @@
+package com.example.prueba2.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.example.prueba2.model.Envio;
+
+@Repository
+public interface  EnvioRepository extends JpaRepository<Envio, Integer> {
+
+}
